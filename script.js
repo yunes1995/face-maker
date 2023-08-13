e92790a59bc11e65884407ca0a17f98feda8fc69
yunes moahammadi <younesm837@gmail.com>
2023-08-14 02:12:19 +0330

    compelet the chose them and change face

--- script.js
+++ script.js
@@ -6,3 +6,52 @@
 //   "#904ae8",
 //   "#ffa711",
 // ];
+
+// color of characters
+
+
+
+// Chane Face :
+
+let faceBtn = document.getElementById("face-btn");
+
+faceBtn.addEventListener("click", changeFace);
+
+let counter = 1;
+let faceType = document.getElementById("face");
+let faceText = document.getElementById("faceText");
+
+function changeFace() {
+    if (counter == 1) { faceType.src = "./img/face-1.png" , faceText.innerText = "Happy" }
+    else if (counter == 2) { faceType.src = "./img/face-2.png" , faceText.innerText = "Tired"}
+    else if (counter == 3) { faceType.src = "./img/face-3.png" , faceText.innerText = "Surprised"}
+    else if (counter == 4) { faceType.src = "./img/face-4.png" , faceText.innerText = "Sad"}
+    else if (counter == 5) { faceType.src = "./img/face-5.png" , faceText.innerText = "Angry"}
+    else if (counter == 6) { faceType.src = "./img/face-6.png" , faceText.innerText = "Confused"}
+    if(counter ==  6) {
+        faceType.src = "./img/face-0.png"
+        counter = 0;
+        return;
+    }
+    counter++;
+}
+
+
+
+// Them : 
+let color1 = document.querySelector(".color1");
+let color2 = document.querySelector(".color2");
+let body = document.querySelector("body");
+let btnColor = document.querySelectorAll(".btn-backgrounds");
+
+function setGradient() {
+    body.style.background = color1.value;
+    btnColor.forEach(items => {
+        items.style.background = color1.value;
+    })
+
+}
+
+
+color1.addEventListener("input", setGradient);
+
